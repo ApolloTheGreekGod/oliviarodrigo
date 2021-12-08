358120382161b13a8a2997cb5401655922b75dfd
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-12-08 19:23:22 +0800

    2.4

---
+++ javascript/imageover.js
@@ -0,0 +1,8 @@
+function BioHover(element) {
+    element.setAttribute('src', 'images/polaroid1.jpg')
+    element.setAttribute('width', '27%')
+}
+function BioUnHover(element) {
+    element.setAttribute('src', 'images/bio-side-image5-pic.png')
+    element.setAttribute('width', '37%')
+}
