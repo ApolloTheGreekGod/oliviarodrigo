e6b27d7698461d49e89ce67d2963f81da6c8b618
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-12-08 19:56:07 +0800

    2.6

--- javascript/imageover.js
+++ javascript/imageover.js
@@ -1,8 +1,15 @@
 function BioHover(element) {
     element.setAttribute('src', 'images/polaroid1.jpg')
     element.setAttribute('width', '27%')
+    document.getElementById('biosideleft').style.top = '140%';
+    document.getElementById('biosideleft').style.left = '2%';
+    document.getElementById('biosideleft').style.transform = 'rotate(-5deg)';
+    
 }
 function BioUnHover(element) {
     element.setAttribute('src', 'images/bio-side-image5-pic.png')
     element.setAttribute('width', '37%')
+    document.getElementById('biosideleft').style.top = '135%';
+    document.getElementById('biosideleft').style.left = '0';
+    document.getElementById('biosideleft').style.transform = 'rotate(0deg)';
 }
